f7e2c86e009c230c15bf8ee1aa6922528c3a537b
Martin Fencl <martin.fencl@gmail.com>
2016-12-26 22:06:45 -0600

    Use shallow nested routing shorthand.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
 
   get 'movies/search', to: 'movies#index', as: :search_movies
   resources :movies do
-    resources :reviews, only: [:new, :create]
+    resources :reviews, shallow: true, except: [:index, :show]
   end
-  resources :reviews, only: [:edit, :update, :destroy]
 
   root 'movies#index'
 end
